6824e653290befddc2fb924523733c55afaf8988
LaluRezaAdrian <64625006+LaluRezaAdrian@users.noreply.github.com>
2020-05-11 16:00:32 +0800

    Create Jobsheet 1C.cpp
    Buatlah 
    program 
    array satu 
    dimensi 
    yang 
    menampil
    kan NIM 
    masing￾masing 
    Mahasis
    wa

---
+++ Jobsheet 1C.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+main () {
+int my_array[10]= {10, 11, 12, 
+13, 14, 15, 16, 17, 18, 19};
+printf("\nLALU REZA 
+ADRIAN");
+printf("\nF1B019080");
+printf("\nKELOMPOK 17");
+printf("\ndata 1 = 
+%d",my_array[0]);
+printf("\ndata 2 = 
+%d",my_array[1]);
+printf("\ndata 3 = 
+%d",my_array[2]);
+printf("\ndata 4 = 
+%d",my_array[3]);
+printf("\ndata 5 = 
+%d",my_array[4]);
+printf("\ndata 6 = 
+%d",my_array[5]);
+printf("\ndata 7 = 
+%d",my_array[6]);
+printf("\ndata 8 = 
+%d",my_array[7]);
+printf("\ndata 9= 
+%d",my_array[8]);
+printf("\ndata 10= 
+%d",my_array[9]);
+return 0;}
