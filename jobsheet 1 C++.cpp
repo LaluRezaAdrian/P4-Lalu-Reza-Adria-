94fafcef0e6262a858dd8c5edf597b50f56fb6f0
LaluRezaAdrian <64625006+LaluRezaAdrian@users.noreply.github.com>
2020-05-15 15:47:07 -0700

    Create jobsheet 1 C++.cpp
    Menampilkan NIM masing-masing mahasiswa dengan array satu dimensi

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+main () {
+
+
+int my_array[10]= {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+cout<<"LALU REZA ADRIAN"<<endl;
+cout<<"F1B019080"<<endl;
+cout<<"KELOMPOK 17"<<endl;
+cout<<"data 1 = "<<my_array[0]<<endl;
+cout<<"data 2 = "<<my_array[1]<<endl;
+cout<<"data 3 = "<<my_array[2]<<endl;
+cout<<"data 4 = "<<my_array[3]<<endl;
+cout<<"data 5 = "<<my_array[4]<<endl;
+cout<<"data 6 = "<<my_array[5]<<endl;
+cout<<"data 7 = "<<my_array[6]<<endl;
+cout<<"data 8 = "<<my_array[7]<<endl;
+cout<<"data 9 = "<<my_array[8]<<endl;
+cout<<"data 10 = "<<my_array[9]<<endl; }
